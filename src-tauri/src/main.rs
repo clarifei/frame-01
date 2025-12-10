61f30209472a97dc900604c7f56b2ef03bb59f18
clarifei <nightcoremosta@gmail.com>
2025-12-11 00:23:06 +0700

    feat(tauri): configure frame plugin options

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -12,7 +12,11 @@ fn main() {
     .plugin(
         FramePluginBuilder::new()
             .titlebar_height(32)
+            .button_width(56)
             .auto_titlebar(true)
+            .snap_overlay_delay_ms(15)
+            .close_hover_bg("rgba(196,43,28,1)")
+            .button_hover_bg("rgba(255,255,255,0.1)")
             .build()
     )
     .plugin(tauri_plugin_http::init())
